5b92070ea94292c1196c9d7c126c5b4f3175da94
eliasdabbas <eliasdabbas@gmail.com>
2019-10-02 18:30:03 +0200

    Create the callback function plot_annual_by_borough

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
+import plotly.graph_objects as go
+from dash.dependencies import Output, Input
 
 data = pd.read_csv('data.csv', parse_dates=['License Creation Date',
                                             'License Expiration Date'])
@@ -15,8 +17,18 @@ app.layout = html.Div([
     dcc.Dropdown(id='borough_dropdown',
                  options=[{'label': b, 'value': b}
                           for b in boroughs]),
-    dcc.Graph()
+    dcc.Graph(id='annual_by_borough_chart')
 ])
+
+@app.callback(Output('annual_by_borough_chart', 'figure'),
+              [Input('borough_dropdown', 'value')])
+def plot_annual_by_borogh(borough):
+    df = data[data['Address Borough'] == borough]
+    timeseries = df.set_index('License Creation Date').resample('A').count()['Industry']
+    fig = go.Figure()
+    fig.add_bar(x=timeseries.index, y=timeseries.values)
+    return fig.to_dict()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
