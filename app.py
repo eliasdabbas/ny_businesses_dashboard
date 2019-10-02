cdbc89ad1effee8bbb7d9752b6fa9f6618228e19
eliasdabbas <eliasdabbas@gmail.com>
2019-10-02 18:51:48 +0200

    Add multi dropdown for industries, & and empty map chart

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from dash.dependencies import Output, Input
 data = pd.read_csv('data.csv', parse_dates=['License Creation Date',
                                             'License Expiration Date'])
 boroughs = data['Address Borough'].dropna().unique().tolist()
+industries = data['Industry'].dropna().unique().tolist()
 
 
 app = dash.Dash(__name__)
@@ -18,7 +19,12 @@ app.layout = html.Div([
                  value='Manhattan',
                  options=[{'label': b, 'value': b}
                           for b in boroughs]),
-    dcc.Graph(id='annual_by_borough_chart')
+    dcc.Graph(id='annual_by_borough_chart'),
+    dcc.Dropdown(id='industry_dropdown',
+                 multi=True,
+                 options=[{'label': i, 'value': i}
+                          for i in industries]),
+    dcc.Graph(id='by_industry_map_chart')
 ])
 
 @app.callback(Output('annual_by_borough_chart', 'figure'),
