70a85b295d209055f007166b359bcc8961892a42
eliasdabbas <eliasdabbas@gmail.com>
2019-10-02 18:04:00 +0200

    Add first component:
    * Set debug=True
    * Add dataset file
    * Get unique values of boroughs
    * Set DropDown options with boroughs

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
+import pandas as pd
+
+data = pd.read_csv('data.csv', parse_dates=['License Creation Date',
+                                            'License Expiration Date'])
+boroughs = data['Address Borough'].dropna().unique().tolist()
+
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([])
+app.layout = html.Div([
+    html.H1('New York Businesses Dashboard'),
+    dcc.Dropdown(id='borough_dropdown',
+                 options=[{'label': b, 'value': b}
+                          for b in boroughs]),
+    dcc.Graph()
+])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
